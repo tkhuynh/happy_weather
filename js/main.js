cdad76cae2a1d8ca1704b46f1fa0b7bef51655f3
tkhuynh <thequest2311@yahoo.com>
2016-05-04 15:48:33 -0700

    add geolocation

--- js/main.js
+++ js/main.js
@@ -1,23 +1,38 @@
 $(function() {
+	console.log($("#map").offset())
+	var cityName;
+	$.getJSON('http://ipinfo.io', function(data){
+		var templateData = {};
+		cityName = data.city + ", " + data.region;
+		var lat = data.loc.split(",")[0];
+		var lon = data.loc.split(",")[1];
+  	var query = "lat=" + lat + "&lon=" + lon;
+  	$.get(url + "forecast?" + query + unit + key, forecast);
+		$.get(url + "weather?" + query + unit + key, currentWeather);
+	});
 	// auto compelte for city
-	var autocompleteCity;
 	google.load("maps", "3.x", {
 		callback: initialize,
 		other_params: 'sensor=false&libraries=places'
+	});
+
+	var url = "http://api.openweathermap.org/data/2.5/";
+	var unit = "&units=imperial";
+	var key = "&appid=c55ec823be46f88fbcf55db70cc8e772";
+	$("#search-form").on("submit", function(e) {
+		cityName = titleize($(this).find("#id_location").val());
+		var templateData = {};
+		e.preventDefault();
+		var query = $(this).serialize();
+		$("input#id_location").val('');
+		$.get(url + "forecast?" + query + unit + key, forecast);
+		$.get(url + "weather?" + query + unit + key, currentWeather);
 	});
 
 	function initialize() {
 		var input = document.getElementById('id_location');
 		var autocomplete = new google.maps.places.Autocomplete(input, {
 			types: ['(cities)']
-		});
-		google.maps.event.addListener(autocomplete, 'place_changed', function() {
-			var place = autocomplete.getPlace();
-			if (!place.geometry) {
-				console.log('no location');
-				return;
-			}
-			autocompleteCity = place.formatted_address;
 		});
 	}
 
@@ -48,93 +63,81 @@ $(function() {
 		});
 	}
 
-	var url = "http://api.openweathermap.org/data/2.5/";
-	var unit = "&units=imperial";
-	var key = "&appid=c55ec823be46f88fbcf55db70cc8e772";
-	$("#search-form").on("submit", function(e) {
-		var templateData = {};
-		e.preventDefault();
-		var query = $(this).serialize();
-		$("input#id_location").val('');
-		$.get(url + "forecast?" + query + unit + key, function(result) {
-			var dataSeries = [];
-			for (var i = 0; i < result.list.length; i += 8) {
-				var dayData = result.list.slice(i, i + 8).map(function(data) {
-					return data.main.temp;
-				});
-				var day = result.list[i].dt_txt.slice(0, 10);
-				dataSeries.push({
-					name: day,
-					data: dayData
-				});
-			}
-			$('#city').text("Welcome to, " + result.city.name);
-			$('#chart-wrapper').highcharts({
+	// forecast
+	function forecast(result) {
+		initMap(result.city.coord.lat, result.city.coord.lon);
+		var dataSeries = [];
+		for (var i = 0; i < result.list.length; i += 8) {
+			var dayData = result.list.slice(i, i + 8).map(function(data) {
+				return data.main.temp;
+			});
+			var day = result.list[i].dt_txt.slice(0, 10);
+			dataSeries.push({
+				name: day,
+				data: dayData
+			});
+		}
+		$('#city').text("Welcome to, " + result.city.name);
+		$('#chart-wrapper').highcharts({
+			title: {
+				text: "Next Five Day Temperature Forecast",
+				x: -20 //center
+			},
+			subtitle: {
+				text: "From " + dataSeries[0].name + " to " + dataSeries[4].name,
+				x: -20
+			},
+			xAxis: {
 				title: {
-					text: "Next Five Day Temperature Forecast",
-					x: -20 //center
+					text: 'Time of the Day'
 				},
-				subtitle: {
-					text: "From " + dataSeries[0].name + " to " + dataSeries[4].name,
-					x: -20
+				categories: ["12AM", "3 AM", "6 AM", "9 AM", "12 PM", "3 PM", "6 PM", "9 PM"]
+			},
+			yAxis: {
+				title: {
+					text: 'Temperature (°F)'
 				},
-				xAxis: {
-					title: {
-						text: 'Time of the Day'
-					},
-					categories: ["00:00", "03:00", "06:00", "09:00", "12:00", "15:00", "18:00", "21:00"]
-				},
-				yAxis: {
-					title: {
-						text: 'Temperature (°C)'
-					},
-					plotLines: [{
-						value: 0,
-						width: 1,
-						color: '#808080'
-					}]
-				},
-				tooltip: {
-					valueSuffix: '°C'
-				},
-				legend: {
-					layout: 'vertical',
-					align: 'right',
-					verticalAlign: 'middle',
-					borderWidth: 0
-				},
-				data: {
-					table: 'datatable'
-				},
-				series: dataSeries
-			});
-			var city = autocompleteCity || result.city.name;
-			$.get(url + "weather?" + query + unit + key, function(currentData) {
-				templateData = {
-					city_name: city,
-					weather_description: titleize(currentData.weather[0].description),
-					cloud_icon_url: "http://openweathermap.org/img/w/" + currentData.weather[0].icon + ".png",
-					temp_icon_url: "images/temperature.png",
-					humidity_icon_url: "images/humidity.png",
-					wind_icon_url: "images/wind.png",
-					temp: Math.round(currentData.main.temp),
-					humidity: currentData.main.humidity,
-					clouds: currentData.clouds.all,
-					wind: currentData.wind.speed
-				};
-				// mustache
-				var template = $('#template').html();
-				Mustache.parse(template); // optional, speeds up future uses
-				var rendered = Mustache.render(template, {
-					currentData: templateData
-				});
-				$('#target').html(rendered);
-			});
-
-			initMap(result.city.coord.lat, result.city.coord.lon);
+				plotLines: [{
+					value: 0,
+					width: 1,
+					color: '#808080'
+				}]
+			},
+			tooltip: {
+				valueSuffix: '°C'
+			},
+			legend: {
+				layout: 'vertical',
+				align: 'right',
+				verticalAlign: 'middle',
+				borderWidth: 0
+			},
+			data: {
+				table: 'datatable'
+			},
+			series: dataSeries
 		});
-	});
-
-
-
+	}
+	// current weather
+	function currentWeather(currentData) {
+		templateData = {
+			city_name: cityName,
+			weather_description: titleize(currentData.weather[0].description),
+			cloud_icon_url: "http://openweathermap.org/img/w/" + currentData.weather[0].icon + ".png",
+			temp_icon_url: "images/temperature.png",
+			humidity_icon_url: "images/humidity.png",
+			wind_icon_url: "images/wind.png",
+			temp: Math.round(currentData.main.temp),
+			humidity: currentData.main.humidity,
+			clouds: currentData.clouds.all,
+			wind: currentData.wind.speed
+		};
+		// mustache
+		var template = $('#template').html();
+		Mustache.parse(template); // optional, speeds up future uses
+		var rendered = Mustache.render(template, {
+			currentData: templateData
+		});
+		$('#target').html(rendered);		
+	}
 });
